284f81d03e993f09390f8c6ae89837b855db2cc9
Pedro Contreras <88257199+PedroContrerasH@users.noreply.github.com>
2021-07-31 20:46:34 -0400

    Create firstpython.py
    Se crea primer archivo del ejercicio
    https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/GitHub1_Getting_Started.md.html?origin=www.coursera.org

---
+++ firstpython.py
@@ -0,0 +1,3 @@
+# Display the Output
+# https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/GitHub1_Getting_Started.md.html?origin=www.coursera.org
+print("New Python file")
